3759fa90ab6bddb30f875e05d9a6fd6e31b1150d
HutoonAljathlan <146876710+HutoonAljathlan@users.noreply.github.com>
2024-03-21 00:01:50 +0300

    Update INSURANCE.sql

--- INSURANCE.sql
+++ INSURANCE.sql
@@ -2,4 +2,7 @@ CREATE TABLE INSURANCE (
     InsuranceID INT PRIMARY KEY,
     Type VARCHAR(100) NOT NULL ,
     InsDetails VARCHAR(100)
+    LicenseNumber VARCHAR(100), 
+    FOREIGN KEY (LicenseNumber) REFERENCES CAR(LicenseNumber)
 );
+
