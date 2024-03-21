434255dcb0de3a7794a94fe06fe740628040402b
0Basmah0 <128868384+0Basmah0@users.noreply.github.com>
2024-03-21 21:35:36 +0300

    Update car_dealerShip.sql

--- car_dealerShip.sql
+++ car_dealerShip.sql
@@ -6,3 +6,12 @@ create table Branch(
   Address_City VARCHAR(100) NOT NULL,
   Address_State VARCHAR(50) NOT NULL,
   Address_Zipcode VARCHAR(20) NOT NULL);
+CREATE TABLE Car
+    LicenseNumber VARCHAR(6) PRIMARY KEY,
+    Price DECIMAL(10,2),
+    Make VARCHAR(10),
+    Year YEAR,
+    Model VARCHAR(49),
+    InsuranceID INT,
+    FOREIGN KEY (InsuranceID) REFERENCES Insurance(InsuranceID)
+);
