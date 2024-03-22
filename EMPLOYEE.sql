4ef11ecede70a805a023fd15775b7bb5b24d03c5
almunasserl <145212368+almunasserl@users.noreply.github.com>
2024-03-22 06:24:29 +0300

    Create EMPLOYEE.sql

---
+++ EMPLOYEE.sql
@@ -0,0 +1,9 @@
+CREATE TABLE EMPLOYEE (
+    EID INT PRIMARY KEY,
+    Ename VARCHAR(100),
+    Ephone VARCHAR(15),
+    Email VARCHAR(100),
+    Dob DATE,
+    BranchID INT,
+    FOREIGN KEY (BranchID) REFERENCES Branch(BranchID)
+);
