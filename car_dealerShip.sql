91a69b1c065562305b17007f0fb7dcf492e0aed5
renad <120721701+Renadyousef@users.noreply.github.com>
2024-03-25 11:33:43 +0300

    car_dealerShip.sql

--- car_dealerShip.sql
+++ car_dealerShip.sql
@@ -2,7 +2,7 @@ create database car_dealerShip;
 use car_dealerShip;
 create table Branch(
   BranchID int NOT NULL PRIMARY KEY,
-  BPhone int, //maybe will set it to not null
+  BPhone int, 
   Address_City VARCHAR(100) NOT NULL,
   Address_State VARCHAR(50) NOT NULL,
   Address_Zipcode VARCHAR(20) NOT NULL);
