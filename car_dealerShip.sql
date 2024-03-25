01dc2a6db55cd6758e0baa225240d0f361008503
danaksu <147039971+danaksu@users.noreply.github.com>
2024-03-25 16:40:25 +0300

    Update car_dealerShip.sql

--- car_dealerShip.sql
+++ car_dealerShip.sql
@@ -47,7 +47,7 @@ CREATE TABLE Associated (
   branch_ID int,
   licence_num VARCHAR(6) PRIMARY KEY,
   FOREIGN KEY (emp_ID) REFERENCES EMPLOYEE(EID),
-  FOREIGN KEY (branch_ID) REFERENCES BRANCH(EID),
+  FOREIGN KEY (branch_ID) REFERENCES BRANCH(BranchID),
   FOREIGN KEY (licence_num) REFERENCES CAR(LicenseNumber)
 );
 
